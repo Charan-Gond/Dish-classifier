cb036cf0dfd4e6c8c16d58083efe6bb4dcf13234
Charan-Gond <charangond42@gmail.com>
2025-06-05 17:35:34 +0530

    backend setup

---
+++ server/Dish-classifier/Dish-classifier/src/main/java/com/chr/Dish_classifier/DishClassifierApplication.java
@@ -0,0 +1,13 @@
+package com.chr.Dish_classifier;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DishClassifierApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DishClassifierApplication.class, args);
+	}
+
+}
---
+++ server/Dish-classifier/Dish-classifier/src/test/java/com/chr/Dish_classifier/DishClassifierApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chr.Dish_classifier;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DishClassifierApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
